7b2cc6441cbcb4c2d420eb1ce8007701a22ffcd7
Randi Stella <RANDINICOLESTELLA@GMAIL.COM>
2023-02-28 10:33:44 -0600

    add book to list

---
+++ app.js
@@ -0,0 +1,56 @@
+// book constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// ui constructor
+function UI() {}
+
+// add book to list
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    
+    //create tr element
+    const row = document.createElement('tr');
+    //insert columns
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X<a></td>
+    `
+
+    list.appendChild(row);
+}
+
+// clear fields 
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+// event listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+
+    // get form values
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+    
+    // instantiate book
+    const book = new Book(title, author, isbn);
+
+    // instatiate UI
+    const ui = new UI();
+    
+    // add book to list
+    ui.addBookToList(book);
+
+    // clear fields 
+    ui.clearFields();
+        
+    e.preventDefault();
+});
